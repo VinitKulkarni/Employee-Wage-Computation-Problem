386f188ea33f7da3f32f9329c0514f21aab033c4
vinit <vinitkkulkarni@gmail.com>
2023-03-31 21:29:01 +0530

    Added : wage calculation

--- EmployeeWageCalculation.java
+++ EmployeeWageCalculation.java
@@ -3,12 +3,18 @@ package com.bridgelabz.Day3PP1;
 public class EmployeeWageCalculation {
     public static void main(String[] args) {
         System.out.println("Employee Wage Calculation");
+        int totalWage = 0;
+        int perHourWage = 20;
+        int fullDayWorkHrs = 8;
 
         int isPresent = (int)(Math.random() * 10)%2;
         if(isPresent == 1){
             System.out.println("Employee is Present");
+            totalWage = (fullDayWorkHrs * perHourWage);
         }else{
             System.out.println("Employee is Absent");
+            totalWage = 0;
         }
+        System.out.println("TOTAL WAGE = " +totalWage);
     }
 }
